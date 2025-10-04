0199df786b64e86d2bc51c8ce40ff51527b5f22a
Rhandie Sales Jr. <76822279+Rheen0@users.noreply.github.com>
2025-10-05 01:33:53 +0800

    feat: added dynamic button component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Grid from "@/components/GridBackground";
+import AnimatedGradientTextDemo from "@/components/Button";
 
 export default function Home() {
   return (
@@ -7,7 +8,12 @@ export default function Home() {
       <Grid />
       {/* foreground content */}
       <div className="relative z-10 text-center px-6">
-        <h1 className="text-4xl font-bold text-slate-800">HELLO, GDG</h1>
+        <h1 className="text-4xl font-bold text-slate-800">OFFICIAL GDG PUP DIGITAL ID</h1>
+        <div className="flex justify-center">
+
+          <AnimatedGradientTextDemo label="Download as PNG" bgColor="bg-blue-600" />
+          <AnimatedGradientTextDemo label="Download as PDF" bgColor="bg-green-600" />
+        </div>
         <p className="mt-3 text-slate-600">Sample Text</p>
       </div>
     </section>
