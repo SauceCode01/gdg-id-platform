f8944411ec53e7f780a976d00bb27b59cf0a7534
Gerald Berongoy <geraldsberongoy@iskolarngbayan.pup.edu.ph>
2025-10-07 09:26:12 +0800

    refactor: update layout and improve component structure in Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,20 +26,24 @@ export default function Home() {
     <>
       <div className="relative flex flex-col items-center justify-center min-h-screen w-full overflow-hidden">
         {/* grid background */}
-        <div className="absolute inset-0 z-0">
+        <div className="fixed inset-0 -z-1">
           <Grid />
         </div>
-        {/* content container */}
-        <div className="w-full relative  flex flex-col my-10 px-4 gap-8">
-          {/* logo part */}
-          <div className="w-full flex flex-row justify-center">
-            <img
-              src="/pinkRedLogo.png"
-              alt="sparky "
-              className="w-20 aspect-auto"
-            />
-          </div>
 
+        <div className="w-[62.87px] h-[62.87px] relative rounded-[37.65px] mt-6 md:hidden">
+          <div className="w-[62.87px] h-[62.87px] absolute left-0 top-0 bg-[#f0f0f3] rounded-[36px] shadow-[0.9908041656px_0.9908041656px_1.9816083312px_0px_rgba(174,174,192,0.40),_-0.6605361254px_-0.6605361254px_4.0953236818px_0px_rgba(255,255,255,1)]" />
+
+          <div className="w-[57.62px] h-[57.62px] absolute left-[2.63px] top-[2.63px] bg-[#eeeeee] rounded-[32.36px] shadow-[inset_0.9908041656px_0.9908041656px_0.6605361254px_0px_rgba(174,174,192,0.20),_inset_-0.6605361254px_-0.6605361254px_0.6605361254px_0px_rgba(255,255,255,0.70)]" />
+          <img
+            src="/sites/landing/stickerBrackets.gif"
+            alt="stickerBrackets"
+            className="w-18 aspect-auto absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2"
+          />
+        </div>
+
+        {/* content container */}
+        <div className=" relative flex flex-col my-4 px-4 gap-8">
+          {/* logo part */}
           {/* heading  */}
           <div className="w-full px-4 font-bold text-3xl text-center">
             Bridging the gap between one two three idk
@@ -71,8 +75,8 @@ export default function Home() {
 
           {/* sparky image */}
           <img
-            src="/sites/landing/SparkyClassicPose.png"
-            alt="sparky "
+            src="/sites/landing/SparkyPose.svg"
+            alt="sparky"
             className="mx-auto w-100 aspect-auto"
           />
         </div>
