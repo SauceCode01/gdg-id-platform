b19525465858d93c741bd05114583acf337cf1cf
Gerald Berongoy <geraldsberongoy@iskolarngbayan.pup.edu.ph>
2025-10-07 19:37:26 +0800

    feat: add dark mode vector designs to enhance visual consistency

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -111,28 +111,48 @@ export default function Home() {
               <span className="ml-2 whitespace-nowrap">Search ID</span>
             </Button>
 
-            {/* vector design*/}
+            {/* vector design - light mode */}
             <img
               src="/sites/landing/Vector-left.svg"
               alt="Vector Left"
-              className="hidden xl:block absolute -top-19 -left-40 "
+              className="hidden xl:block dark:xl:hidden absolute -top-19 -left-40 "
             />
             <img
               src="/sites/landing/Vector-right.svg"
               alt="Vector right"
-              className="hidden xl:block absolute -right-30 -top-18"
+              className="hidden xl:block dark:xl:hidden absolute -right-30 -top-18"
             />
-
             <img
               src="/sites/landing/Vector-left.svg"
               alt="Vector Left"
-              className="hidden xl:block absolute top-20 left-18 scale-x-[1] scale-y-[-1] rotate-270 "
+              className="hidden xl:block dark:xl:hidden absolute top-20 left-18 scale-x-[1] scale-y-[-1] rotate-270 "
             />
-
             <img
               src="/sites/landing/Vector-left.svg"
               alt="Vector Left"
-              className="hidden xl:block absolute top-20 right-15 scale-x-[1]  rotate-270 "
+              className="hidden xl:block dark:xl:hidden absolute top-20 right-15 scale-x-[1]  rotate-270 "
+            />
+
+            {/* vector design - dark mode */}
+            <img
+              src="/sites/landing/Vector-left-dark.svg"
+              alt="Vector Left"
+              className="hidden dark:xl:block absolute -top-19 -left-40 "
+            />
+            <img
+              src="/sites/landing/Vector-right-dark.svg"
+              alt="Vector right"
+              className="hidden dark:xl:block absolute -right-30 -top-18"
+            />
+            <img
+              src="/sites/landing/Vector-left-dark.svg"
+              alt="Vector Left"
+              className="hidden dark:xl:block absolute top-20 left-18 scale-x-[1] scale-y-[-1] rotate-270 "
+            />
+            <img
+              src="/sites/landing/Vector-left-dark.svg"
+              alt="Vector Left"
+              className="hidden dark:xl:block absolute top-20 right-15 scale-x-[1]  rotate-270 "
             />
 
             {/* floating stickers — only show on large screens */}
