e47e18f1792ce4072b2d4bdc490365612bea3d22
Sauce <111296942+SauceCode01@users.noreply.github.com>
2025-10-14 12:36:29 +0800

    #94 added collaborators section on about page (#95)
    * #94 added project contributors on about page
    
    * changed y-padding of top section on about page
    
    
    closes #94

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -7,15 +7,17 @@ import Grid from "@/components/GridBackground";
 import GlowBlobs from "@/components/GlowBlobs";
 import { cn } from "@/lib/utils";
 import { useGlobalContext } from "@/providers/GlobalContextProvider";
+import Link from "next/link";
+import { useState } from "react";
 
 const AboutPage = () => {
   const router = useRouter();
   const handleCta = () => router.push("/");
 
-  const {isDarkMode} = useGlobalContext();
+  const { isDarkMode } = useGlobalContext();
 
   return (
-    <div className="relative overflow-hidden ">
+    <div className="relative  z-0">
       {/* background grid */}
       <Grid />
 
@@ -26,10 +28,10 @@ const AboutPage = () => {
 
       {/* floating stickers — only show on large screens */}
       <div
-        className="hidden lg:flex absolute left-[100px] top-[60%] -translate-y-1/2 
+        className="hidden lg:flex absolute left-[100px] top-[50vh] -translate-y-1/2 
         w-[105px] h-[105px] items-center justify-center rounded-full bg-white/90 
           backdrop-blur-sm 
-         shadow-[0_0_20px_5px_rgba(255,255,255,0.6)] border-4 border-white/60 z-20 rotate-20"
+         shadow-[0_0_20px_5px_rgba(255,255,255,0.6)] border-4 border-white/60  rotate-20 -z-10"
       >
         <img
           src="/sites/about/stickerBrackets.gif"
@@ -42,7 +44,7 @@ const AboutPage = () => {
         className="hidden lg:flex absolute right-[100px] top-[60px]
         w-[105px] h-[105px] items-center justify-center rounded-full bg-white/90 
        shadow-[0_0_20px_5px_rgba(255,255,255,0.6)] border-4 border-white/60 backdrop-blur-sm 
-         z-20 -rotate-12"
+          -rotate-12 -z-10"
       >
         <img
           src="/sites/about/stickerBrackets.gif"
@@ -53,15 +55,14 @@ const AboutPage = () => {
 
       {/* content */}
       <div className="relative z-10 flex flex-col items-center justify-center px-4 sm:px-6 lg:px-10 pt-10 pb-10 -translate-y-4 lg:-translate-y-10">
-        <div className="flex flex-col lg:flex-row items-center justify-center w-full max-w-7xl gap-0 relative lg:my-16">
+        <div className="flex flex-col lg:flex-row items-center justify-center w-full max-w-7xl gap-0 relative lg:my-24">
           {/* Mobile Layout — Titles above Image */}
           <div className="block lg:hidden text-center">
             <h1
               className={cn(
                 "font-bold  text-4xl xs:text-5xl sm:text-6xl leading-none mb-1 text-gdg-blue mt-8",
-                "drop-shadow-[0_0_2px_var(--color-gdg-blue)]",
+                "drop-shadow-[0_0_2px_var(--color-gdg-blue)]"
               )}
-               
             >
               Project Vision
             </h1>
@@ -87,7 +88,7 @@ const AboutPage = () => {
               <h1
                 className={cn(
                   "font-bold text-6xl leading-none mb-0 text-[#4285F4] hover:scale-x-105 transition-all duration-200 text-center",
-                "drop-shadow-[0_0_2px_var(--color-gdg-blue)]",
+                  "drop-shadow-[0_0_2px_var(--color-gdg-blue)]"
                 )}
               >
                 Project Vision
@@ -124,8 +125,242 @@ const AboutPage = () => {
           </div>
         </div>
       </div>
+
+      {/* contributors section */}
+      <div className="w-full relative  z-10  max-w-7xl mx-auto px-4  mt-16 md:mt-0 ">
+        {/* main header */}
+        <div className="w-full flex flex-row justify-center mb-4 md:mb-8 text-center">
+          <h1
+            className={cn(
+              "font-bold  text-4xl xs:text-5xl sm:text-6xl leading-none mb-1 text-gdg-blue ",
+              "drop-shadow-[0_0_2px_var(--color-gdg-blue)]"
+            )}
+          >
+            Project Contributors
+          </h1>
+        </div>
+
+        {/* per team section */}
+        <div className="flex flex-col w-full gap-16">
+          {CONTRIBUTORS.map((team) => (
+            <>
+              <div className="flex flex-col w-full gap-2 md:gap-4">
+                {/* team header */}
+                <div
+                  className={cn(
+                    "font-bold  text-2xl xs:text-2xl sm:text-3xl leading-none",
+                    "w-full text-center",
+                    team.textColor === "blue" && "text-gdg-blue drop-shadow-[0_0_2px_var(--color-gdg-blue)]",
+                    team.textColor === "orange" && "text-gdg-orange drop-shadow-[0_0_2px_var(--color-gdg-orange)]",
+                    team.textColor === "green" && "text-gdg-green drop-shadow-[0_0_2px_var(--color-gdg-green)]",
+                    team.textColor === "red" && "text-gdg-red drop-shadow-[0_0_2px_var(--color-gdg-red)]"
+                  )}
+                >
+                  {team.group}
+                </div>
+                {/* team members */}
+                <div className="  w-full  grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:flex lg:flex-row lg:justify-center lg:grid-cols-none  gap-0">
+                  {team.members.map((member, index) => (
+                    <MemberCard key={index} member={member} />
+                  ))}
+                </div>
+              </div>
+            </>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
 
 export default AboutPage;
+
+const MemberCard = ({ member }: { member: MemberType }) => {
+
+   const [rotation, setRotation] = useState(0);
+
+  const handleMouseEnter = () => {
+    const randomDeg = Math.random() * 2 - 1; // random between -20° and +20°
+    setRotation(randomDeg);
+  };
+
+  const handleMouseLeave = () => {
+    setRotation(0); // reset when hover ends
+  };
+
+
+  return (
+    <div
+      className="w-full flex-1  max-w-none lg:max-w-xs relative hover:scale-105 transition-all duration-200 "
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      style={{
+        transform: `rotate(${rotation}deg)`, 
+      }}
+    >
+      {/* card template */}
+      <img
+        src={member.image}
+        alt="contributor card"
+        className="w-full h-auto object-cover"
+      />
+
+      <div className="w-full flex flex-row justify-center gap-[3.5%] absolute bottom-[18.5%] left-1/2 -translate-x-1/2 ">
+        {member.fb && (
+          <Link
+            className=" w-[6%] aspect-square hover:scale-105 hover:drop-shadow-sm hover:drop-shadow-gdg-orange/20 transition-all duration-200 "
+            href={member.fb}
+          >
+            <img
+              src="/contributors/fb.svg"
+              alt="contributor card"
+              className=" w-full h-full object-cover "
+            />
+          </Link>
+        )}
+        {member.ig && (
+          <Link
+            className=" w-[6%] aspect-square hover:scale-105 hover:drop-shadow-sm hover:drop-shadow-gdg-orange/20 transition-all duration-200 "
+            href={member.ig}
+          >
+            <img
+              src="/contributors/ig.svg"
+              alt="contributor card"
+              className=" w-full h-full object-cover "
+            />
+          </Link>
+        )}
+        {member.linkedin && (
+          <Link
+            className=" w-[6%] aspect-square hover:scale-105 hover:drop-shadow-sm hover:drop-shadow-gdg-orange/20 transition-all duration-200 "
+            href={member.linkedin}
+          >
+            <img
+              src="/contributors/linkedin.svg"
+              alt="contributor card"
+              className=" w-full h-full object-cover "
+            />
+          </Link>
+        )}
+      </div>
+    </div>
+  );
+};
+
+type MemberType = {
+  name: string;
+  image: string;
+  role: string;
+  fb?: string;
+  ig?: string;
+  linkedin?: string;
+};
+
+type TeamType = {
+  group: string;
+  textColor: string;
+  members: MemberType[];
+};
+
+const CONTRIBUTORS: TeamType[] = [
+  {
+    group: "UI/UX Team",
+    textColor: "red",
+    members: [
+      {
+        name: "Jedia Nicole Sagun",
+        image: "/contributors/cards/jedia.png",
+        role: "UI/UX Designer",
+        fb: "https://www.facebook.com/jedianicole.sagun/",
+        ig: "https://www.instagram.com/_jnsagun/",
+        linkedin: "https://www.linkedin.com/in/jnsagun/",
+      },
+      {
+        name: "Kassandra Rychelle Balona",
+        image: "/contributors/cards/kassandra.png",
+        role: "UI/UX Designer",
+        fb: "https://www.facebook.com/kasrych",
+        ig: "https://www.instagram.com/kasrych/",
+        linkedin: "https://www.linkedin.com/in/kasrych/",
+      },
+      {
+        name: "Joyrel Baladjay",
+        image: "/contributors/cards/joyrel.png",
+        role: "UI/UI Designer",
+        fb: "https://www.facebook.com/joyyirel",
+        ig: "https://www.instagram.com/joyyirel/",
+        linkedin: "https://www.linkedin.com/in/joyrel-baladjay-421371195/",
+      },
+      {
+        name: "Kacey Michaela Solis",
+        image: "/contributors/cards/kacey.png",
+        role: "UI/UI Designer",
+        fb: "https://www.facebook.com/kayex13/",
+        ig: "https://www.instagram.com/miikayella/",
+        linkedin: "https://www.linkedin.com/in/kacey-michaela-solis-a76577312/",
+      },
+    ],
+  },
+  {
+    group: "Web Development Team",
+    textColor: "orange",
+    members: [
+      {
+        name: "Erwin Daguinotas",
+        image: "/contributors/cards/erwin.png",
+        role: "Full Stack Developer",
+        fb: "https://www.facebook.com/DaguinotasErwin",
+        ig: "https://www.instagram.com/winnnwnwnwn/",
+        linkedin: "https://www.linkedin.com/in/erwin-daguinotas/",
+      },
+      {
+        name: "Rhandie Sales Jr",
+        image: "/contributors/cards/rhandie.png",
+        role: "Full Stack Developer",
+        fb: "https://www.facebook.com/rhandie.sales.1",
+        ig: "https://www.instagram.com/rhandiejrr/",
+        linkedin: "https://www.linkedin.com/in/rhandie-sales/",
+      },
+      {
+        name: "Gerald Berongoy",
+        image: "/contributors/cards/gerald.png",
+        role: "Full Stack Developer",
+        fb: "https://www.facebook.com/gerald.berongoy0904#",
+        ig: "https://www.instagram.com/g333rald",
+        linkedin: "https://www.linkedin.com/in/geraldberongoy",
+      },
+      {
+        name: "Daniella Simara",
+        image: "/contributors/cards/daniella.png",
+        role: "Full Stack Developer",
+        fb: "https://www.facebook.com/share/1Ee1wU4gyA/",
+        ig: "https://www.instagram.com/d.simara_?igsh=dzAyems5YXg5Z2Zk",
+        linkedin: "www.linkedin.com/in/daniella-simara-908b8229a",
+      },
+    ],
+  },
+  {
+    group: "Technology Officers",
+    textColor: "green",
+    members: [
+      {
+        name: "Nyzel Cayat",
+        image: "/contributors/cards/nyzel.png",
+        role: "Deputy Chief Technology Officer",
+      },
+      {
+        name: "Aurold John Sadullo",
+        image: "/contributors/cards/aj.png",
+        role: "Deputy Chief Technology Officer",
+      },
+      {
+        name: "Carlos Jerico Dela Torre",
+        image: "/contributors/cards/jerico.png",
+        role: "Chief Technology Officer",
+        fb: "https://www.facebook.com/2iLiTE/",
+        ig: "https://www.instagram.com/__rikin__/",
+        linkedin: "https://www.linkedin.com/in/delatorrecj/",
+      },
+    ],
+  },
+];
